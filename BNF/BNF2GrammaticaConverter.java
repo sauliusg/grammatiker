dacf75312290dac0abab7cdb5f171cc0369998b7
saulius <saulius@8a4994de-5b18-4c12-b7b7-b390355806d6>
2015-07-07 14:15:02 +0000

    trunk/ (saulius@koala.ibt.lt) Adding children node to the BNF production node, and printing out their list.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/grammatiker/trunk@73 8a4994de-5b18-4c12-b7b7-b390355806d6

--- BNF/BNF2GrammaticaConverter.java
+++ BNF/BNF2GrammaticaConverter.java
@@ -7,6 +7,7 @@
 
 import java.io.*;
 
+
 import net.percederberg.grammatica.parser.Node;
 import net.percederberg.grammatica.parser.Production;
 import net.percederberg.grammatica.parser.Token;
@@ -22,11 +23,18 @@ class BNF2GrammaticaConverter extends BNFAnalyzer {
     protected void childBnf( Production node, Node child ) throws ParseException
     {
         System.out.println( "Procesisng children of the 'BNF' node." );
+        super.childBnf( node, child );
     }
 
     protected Node exitBnf( Production node ) throws ParseException
     {
         System.out.println( "Reduced the 'BNF' node." );
+        int children_count = node.getChildCount();
+        System.out.println( "children_count = " + children_count );
+        for( int i = 0; i < children_count; i++ ) {
+            Node child = node.getChildAt( i );
+            System.out.println( "child = " + child.toString() );
+        }
         return null;
     }
 
