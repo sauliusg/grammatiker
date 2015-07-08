2a43fb33157eed514ef6f6547592a640265c4b19
saulius <saulius@8a4994de-5b18-4c12-b7b7-b390355806d6>
2015-07-08 10:15:01 +0000

    trunk/ (saulius@koala.ibt.lt) Printing out full BNF rules.
    git-svn-id: svn+ssh://saulius-grazulis.lt/home/saulius/svn-repositories/grammatiker/trunk@77 8a4994de-5b18-4c12-b7b7-b390355806d6

--- BNF/BNF2GrammaticaConverter.java
+++ BNF/BNF2GrammaticaConverter.java
@@ -25,6 +25,46 @@ class BNF2GrammaticaConverter extends BNFAnalyzer {
         super.childBnf( node, child );
     }
 
+    private void printTerm( Node node )
+    {
+        Node term_def = node.getChildAt( 0 );
+
+        if( term_def.getName().equals( "rule_name" )) {
+            Token rule_name_token = (Token)term_def.getChildAt( 1 );
+            System.out.print( rule_name_token.getImage() );
+        } else
+        if( term_def.getName().equals( "literal" )) {
+            Token rule_name_token = (Token)term_def.getChildAt( 0 );
+            String value = rule_name_token.getImage();
+            System.out.print( value );
+        } else {
+            System.out.print( "UNKNOWN-TERM" );
+        }
+            
+    }
+
+    private void printTermList( Node node )
+    {
+        Node term = node.getChildAt( 0 );
+        printTerm( term );
+        if( node.getChildCount() > 1 ) {
+            System.out.print( " " );
+            Node tail_list = node.getChildAt( 1 );
+            printTermList( tail_list );
+        }
+    }
+
+    private void printExpression( Node node )
+    {
+        Node term_list = node.getChildAt( 0 );
+        printTermList( term_list );
+        if( node.getChildCount() > 1 ) {
+            System.out.print( " | " );
+            Node tail_expression = node.getChildAt( 2 );
+            printExpression( tail_expression );
+        }
+    }
+
     private void printProduction( Node node )
     {
         Node rule_expr = node.getChildAt(2);
@@ -32,6 +72,9 @@ class BNF2GrammaticaConverter extends BNFAnalyzer {
         Token rule_name_token = (Token)rule_name_node.getChildAt(1);
 
         System.out.print( rule_name_token.getImage() + " = " );
+
+        printExpression( rule_expr );
+
         System.out.println( "" );
     }
 
@@ -53,9 +96,9 @@ class BNF2GrammaticaConverter extends BNFAnalyzer {
     {
         System.out.println( "%header%" );
         System.out.println( "GRAMMARTYPE = \"LL\"" );
-        System.out.println( "%tokens%" );
+        System.out.println( "\n%tokens%" );
         // this.printTokens( node );
-        System.out.println( "%productions%" );
+        System.out.println( "\n%productions%" );
         this.printProductions( node, "" );
         return null;
     }
